d5c45faeb2a7563822f3a9b6b7f32a2deda91eeb
giahuynguyen2005 <huy.nguyengia2005@gmail.com>
2024-05-08 02:33:28 +0700

    camera added

--- Coding/IntelliJ Code/CrazyPutting/src/engineTester/MainGameLoop.java
+++ Coding/IntelliJ Code/CrazyPutting/src/engineTester/MainGameLoop.java
@@ -1,5 +1,6 @@
 package engineTester;
 
+import entities.Camera;
 import entities.Entity;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
@@ -19,37 +20,102 @@ public class MainGameLoop {
 
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 
 		float[] vertices = {
-				-0.5f,0.5f,0,	//V0
-				-0.5f,-0.5f,0,	//V1
-				0.5f,-0.5f,0,	//V2
-				0.5f,0.5f,0		//V3
-		};
+				-0.5f,0.5f,0,
+				-0.5f,-0.5f,0,
+				0.5f,-0.5f,0,
+				0.5f,0.5f,0,
 
-		int[] indices = {
-				0,1,3,	//Top left triangle (V0,V1,V3)
-				3,1,2	//Bottom right triangle (V3,V1,V2)
+				-0.5f,0.5f,1,
+				-0.5f,-0.5f,1,
+				0.5f,-0.5f,1,
+				0.5f,0.5f,1,
+
+				0.5f,0.5f,0,
+				0.5f,-0.5f,0,
+				0.5f,-0.5f,1,
+				0.5f,0.5f,1,
+
+				-0.5f,0.5f,0,
+				-0.5f,-0.5f,0,
+				-0.5f,-0.5f,1,
+				-0.5f,0.5f,1,
+
+				-0.5f,0.5f,1,
+				-0.5f,0.5f,0,
+				0.5f,0.5f,0,
+				0.5f,0.5f,1,
+
+				-0.5f,-0.5f,1,
+				-0.5f,-0.5f,0,
+				0.5f,-0.5f,0,
+				0.5f,-0.5f,1
+
 		};
 
 		float[] textureCoords = {
+
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
 				0,0,
 				0,1,
 				1,1,
 				1,0
+
+
+		};
+
+		int[] indices = {
+				0,1,3,
+				3,1,2,
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
 		};
 
 		RawModel model = loader.loadToVAO(vertices, textureCoords,indices);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("lol"));
 		TexturedModel staticModel = new TexturedModel(model, texture);
-		Entity entity = new Entity(staticModel, new Vector3f(-1,0,0),0,0,0,1);
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+		Camera camera = new Camera();
+
 
 		while(!Display.isCloseRequested()){
 			//game logic
+			entity.increaseRotation(1f,1f,0);
+			camera.move();
 			renderer.prepare();
 			shader.start();
+			shader.loadViewnMatrix(camera);
 			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
