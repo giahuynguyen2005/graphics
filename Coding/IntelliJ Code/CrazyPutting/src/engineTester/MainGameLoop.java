a8ddf17a9d87b00e90f9bf4203a379d213dbb9e3
giahuynguyen2005 <huy.nguyengia2005@gmail.com>
2024-05-10 02:17:58 +0700

    lighting added

--- Coding/IntelliJ Code/CrazyPutting/src/engineTester/MainGameLoop.java
+++ Coding/IntelliJ Code/CrazyPutting/src/engineTester/MainGameLoop.java
@@ -2,6 +2,7 @@ package engineTester;
 
 import entities.Camera;
 import entities.Entity;
+import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
@@ -21,22 +22,24 @@ public class MainGameLoop {
 		Renderer renderer = new Renderer(shader);
 
 
+		RawModel model = OBJLoader. loadOBJModel("dragon", loader);
 
-		RawModel model = OBJLoader. loadOBJModel("stall", loader);
-
-		ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
+		ModelTexture texture = new ModelTexture(loader.loadTexture("lebron"));
 		TexturedModel staticModel = new TexturedModel(model, texture);
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-25),0,0,0,1);
+		Light light = new Light(new Vector3f(0,10,-25), new Vector3f(1,1,1));
 		Camera camera = new Camera();
+
 
 
 		while(!Display.isCloseRequested()){
 			//game logic
-			entity.increaseRotation(0,1f,0);
+			entity.increaseRotation(0,0.1f,0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
-			shader.loadViewnMatrix(camera);
+			shader.loadLight(light);
+			shader.loadViewMatrix(camera);
 			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
